7a67f73e4c2f111a50a6818dbf6e10497eb18532
chirauraNoSakusha <chiraura@outlook.jp>
2014-02-17 00:27:02 +0900

    配布用書庫作成スクリプトを整理
    ついでに names.txt をトップディレクトリに移した。
    これで、README の通り実行すれば names.txt が読み込まれる。

--- script/makeJar.sh
+++ script/makeJar.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-SOURCE=${SOURCE:=$(dirname ${0})/../src}
-DESTINATION=${DESTINATION:=${HOME}/tmp}
+TOP=${TOP:=$(dirname ${0})/..}
+DESTINATION=${DESTINATION:=${HOME}/tmp/$(date +%F-%H-%M-%S)}
 MAIN=${MAIN:=nippon.kawauso.chiraura.a.A}
 TEST_CHECKER=${TEST_CHECKER:=nippon.kawauso.chiraura.util.TestChecker}
 MOSAICER=${MOSAICER:=nippon.kawauso.chiraura.util.PeerMosaicer}
@@ -11,86 +11,96 @@ UNMOSAICER=${UNMOSAICER:=nippon.kawauso.chiraura.util.PeerUnmosaicer}
 
 AUTHOR_HOST=${AUTHOR_HOST:=""}
 
-DIR=$(date +%F-%H-%M-%S)
-
-if [ -e ${DESTINATION}/${DIR} ]; then
-    rm -rf ${DESTINATION}/${DIR}
+if [ -e ${DESTINATION} ]; then
+    rm -rf ${DESTINATION}
 fi
 
-echo "コンパイル開始"
-CLASS=${DESTINATION}/${DIR}/bin
+
+echo "コンパイル開始。"
+SOURCE=${TOP}/src
+CLASS=${DESTINATION}/bin
 
 mkdir -p ${CLASS}
 
 javac -sourcepath ${SOURCE} -d ${CLASS} ${SOURCE}/$(echo ${MAIN} | sed 's/\./\//g').java
 
 
-echo "JAR 作成開始"
-RAW=${DESTINATION}/${DIR}/chiraura.jar
+echo "JAR 作成開始。"
+JAR=${DESTINATION}/chiraura.jar
 
-jar cfe ${RAW} ${MAIN} -C ${CLASS} .
+jar cfe ${JAR} ${MAIN} -C ${CLASS} .
 
 
-echo "状態検査開始"
+echo "状態検査開始。"
+# 事後検査なのは、余計なことせずに、 余計なクラスを jar に入れずに済むから。
+
 javac -sourcepath ${SOURCE} -d ${CLASS} ${SOURCE}/$(echo ${TEST_CHECKER} | sed 's/\./\//g').java
 
 TEST_STATE=$(java -classpath ${CLASS} ${TEST_CHECKER})
-
 if [ ${TEST_STATE} = "true" ]; then
     echo "非制限状態です。"
-    exit 1
+    echo "ここまでで終了します。"
+    exit 0
 fi
 
-JAR=${DESTINATION}/${DIR}/chiraura.jar
 
-if [ -z "${AUTHOR_HOST}" ]; then
-    echo "環境変数 AUTHOR_HOST として 初期個体が指定されていません。"
+echo "初期個体の準備開始。"
+PEER_SOURCE=${TOP}/peers.txt
+PEER=${DESTINATION}/peers.txt
+
+javac -sourcepath ${SOURCE} -d ${CLASS} ${SOURCE}/$(echo ${MOSAICER} | sed 's/\./\//g').java
+javac -sourcepath ${SOURCE} -d ${CLASS} ${SOURCE}/$(echo ${UNMOSAICER} | sed 's/\./\//g').java
+
+if [ -e ${PEER_SOURCE} ]; then
+    DECODED=$(java -classpath ${CLASS} ${UNMOSAICER} $(cat ${PEER_SOURCE} | sed 's/^\^\(.*\)$/\1/'))
+
+    if [ -n "${AUTHOR_HOST}" ] && [ "_${AUTHOR_HOST}" != "_${DECODED}" ]; then
+        rm ${PEER_SOURCE}
+    fi
+fi
+
+if ! [ -e ${PEER_SOURCE} ]; then
+    if [ -z "${AUTHOR_HOST}" ]; then
+        echo "初期個体が環境変数 AUTHOR_HOST としても ${PEER_SOURCE} としても指定されていません。"
+        echo "ここまでで終了します。"
+        exit
+    fi
+
+    # 改行は環境に依存するので念のため改行無し。
+    printf "^"$(java -classpath ${CLASS} ${MOSAICER} ${AUTHOR_HOST}) > ${PEER_SOURCE}
+fi
+
+cp ${PEER_SOURCE} ${PEER}
+
+
+echo "名無しの準備開始。"
+NAME_SOURCE=${TOP}/names.txt
+NAME=${DESTINATION}/names.txt
+
+if ! [ -e ${NAME_SOURCE} ]; then
+    echo "デフォルト名無しが ${NAME_SOURCE} として指定されていません。"
     echo "ここまでで終了します。"
     exit
 fi
 
-echo "初期個体の準備開始"
-PEER=${DESTINATION}/${DIR}/peers.txt
-
-javac -sourcepath ${SOURCE} -d ${CLASS} ${SOURCE}/$(echo ${MOSAICER} | sed 's/\./\//g').java
-
-javac -sourcepath ${SOURCE} -d ${CLASS} ${SOURCE}/$(echo ${UNMOSAICER} | sed 's/\./\//g').java
-
-if [ -e $(dirname "${0}")/peers.txt ]; then
-    DECODED=$(java -classpath ${CLASS} ${UNMOSAICER} $(cat $(dirname "${0}")/peers.txt | sed 's/^\^\(.*\)$/\1/'))
-
-    if ! [ "_${AUTHOR_HOST}" = "_${DECODED}" ]; then
-        rm $(dirname "${0}")/peers.txt
-    fi
-fi
-
-if ! [ -e $(dirname "${0}")/peers.txt ]; then
-    # 改行は環境に依存するので念のため改行無し。
-    printf "^"$(java -classpath ${CLASS} ${MOSAICER} ${AUTHOR_HOST}) > $(dirname "${0}")/peers.txt
-fi
-
-cp $(dirname "${0}")/peers.txt ${PEER}
+# 改行を CRLF に。長い物に巻かれる。改行として解釈されなくなることはないし。
+sed 's/$/\r/' ${NAME_SOURCE} > ${NAME}
 
 
-echo "名無しの準備開始"
-NAME=${DESTINATION}/${DIR}/names.txt
-if [ -e $(dirname "${0}")/names.txt ]; then
-    # CRLF に。長い物に巻かれる。改行として解釈されなくなることはないし。
-    sed 's/$/\r/' $(dirname "${0}")/names.txt > ${NAME}
-fi
-
-
-echo "書庫作成開始"
-OUTPUT=${DESTINATION}/${DIR}/chiraura_$(date +%F).zip
+echo "書庫作成開始。"
+OUTPUT=${DESTINATION}/chiraura_$(date +%F).zip
 
 zip -q9 ${OUTPUT} -j ${JAR} ${PEER} ${NAME}
 
 
-echo "チェックサム計算開始"
-SUM=${DESTINATION}/${DIR}/chiraura.sum
+echo "チェックサム計算開始。"
+SUM=${DESTINATION}/chiraura.sum
 
 rm -f ${SUM}
 for i in md5sum sha1sum; do
     echo ${i} >> ${SUM}
     ${i} ${OUTPUT} >> ${SUM}
 done
+
+
+echo "${OUTPUT} の作成が完了しました。"
