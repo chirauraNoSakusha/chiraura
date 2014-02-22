d92360c2374de64756e4703b398e79669b7fb096
chirauraNoSakusha <chiraura@outlook.jp>
2014-02-22 12:24:45 +0900

    通信制限を行うクラスを追加

---
+++ src/nippon/kawauso/chiraura/messenger/ConstantTrafficLimiter.java
@@ -0,0 +1,291 @@
+/**
+ * 
+ */
+package nippon.kawauso.chiraura.messenger;
+
+import java.net.InetSocketAddress;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 一定期間の一定量・一定回数で制限。
+ * 通信終了後、一定期間後にサイズ指定無しの nextSleep を呼ばないと破棄できない。
+ * @author chirauraNoSakusha
+ */
+final class ConstantTrafficLimiter implements TrafficLimiter {
+
+    private static final Logger LOG = Logger.getLogger(ConstantTrafficLimiter.class.getName());
+
+    // データをまとめるためだけ。
+    private static final class DateAndSize {
+        private final long date;
+        private final long size;
+
+        private DateAndSize(final long date, final long size) {
+            this.date = date;
+            this.size = size;
+        }
+    }
+
+    private static final class Sum {
+        private final Lock lock;
+        private long sum;
+        private final Deque<DateAndSize> entries;
+
+        private Sum() {
+            this.lock = new ReentrantLock();
+            this.sum = 0;
+            this.entries = new LinkedList<>();
+        }
+
+        private void lock() throws InterruptedException {
+            this.lock.lockInterruptibly();
+        }
+
+        private void unlock() {
+            this.lock.unlock();
+        }
+
+        private boolean isEmpty() {
+            return this.sum == 0;
+        }
+
+        private long getSize() {
+            return this.sum;
+        }
+
+        private int getCount() {
+            return this.entries.size();
+        }
+
+        private long getFirstDate() {
+            return this.entries.peekFirst().date;
+        }
+
+        private long getLastDate() {
+            return this.entries.peekLast().date;
+        }
+
+        // 末尾に追加。
+        private void add(final long date, final long size) {
+            this.entries.offerLast(new DateAndSize(date, size));
+            this.sum += size;
+        }
+
+        // 先頭から削除。
+        private void poll() {
+            final DateAndSize oldest = this.entries.pollFirst();
+            if (oldest != null) {
+                this.sum -= oldest.size;
+            }
+        }
+
+        // 時刻が古いものを削除。
+        private void trim(final long deadline) {
+            while (true) {
+                final DateAndSize oldest = this.entries.peekFirst();
+                if (oldest == null || deadline < oldest.date) {
+                    break;
+                }
+                poll();
+            }
+        }
+    }
+
+    // 参照。
+    private final long duration;
+    private final long sizeLimit;
+    private final int countLimit;
+    private final long penalty;
+
+    // 保持。
+    private final Lock lock;
+    private final Map<InetSocketAddress, Sum> sums;
+
+    ConstantTrafficLimiter(final long duration, final long sizeLimit, final int countLimit, final long penalty) {
+        if (duration < 0) {
+            throw new IllegalArgumentException("Negative duration ( " + sizeLimit + " ).");
+        } else if (sizeLimit < 0) {
+            throw new IllegalArgumentException("Negative size limit ( " + sizeLimit + " ).");
+        } else if (countLimit < 0) {
+            throw new IllegalArgumentException("Negative count limit ( " + countLimit + " ).");
+        } else if (penalty < 0) {
+            throw new IllegalArgumentException("Negative penalty ( " + penalty + " ).");
+        }
+
+        this.duration = duration;
+        this.sizeLimit = sizeLimit;
+        this.countLimit = countLimit;
+        this.penalty = penalty;
+
+        this.lock = new ReentrantLock();
+        this.sums = new HashMap<>();
+    }
+
+    boolean isEmpty() {
+        this.lock.lock();
+        try {
+            return this.sums.isEmpty();
+        } finally {
+            this.lock.unlock();
+        }
+    }
+
+    @Override
+    public long nextSleep(final long size, final InetSocketAddress destination) throws InterruptedException {
+        long sleep = 0L;
+
+        Sum sum = null;
+        try { // 個別のロック。
+            this.lock.lockInterruptibly();
+            try { // 全体のロック。
+                sum = this.sums.get(destination);
+                if (sum == null) {
+                    sum = new Sum();
+                    this.sums.put(destination, sum);
+                }
+
+                // ロック結合。
+                sum.lock();
+            } finally {
+                this.lock.unlock();
+            }
+
+            // 通信した時刻とは違うけど大した問題にはならないはず。
+            // 一応、ロック後に取得しているので、sum.entries 内では確実に時刻順で並ぶ。
+            final long cur = System.currentTimeMillis();
+            sum.trim(cur - this.duration);
+            sum.add(cur, size);
+            if (this.sizeLimit <= sum.getSize() || this.countLimit <= sum.getCount()) {
+                sleep = sum.getFirstDate() + this.duration - cur // 一番古いのが削除されるまでの時間。
+                        + this.penalty;
+            }
+        } finally {
+            if (sum != null) {
+                sum.unlock();
+            }
+        }
+
+        return sleep;
+    }
+
+    @Override
+    public long nextSleep(final InetSocketAddress destination) throws InterruptedException {
+        long sleep = 0L;
+        boolean empty = false;
+
+        Sum sum = null;
+        try { // 個別のロック。
+            this.lock.lockInterruptibly();
+            try { // 全体のロック。
+                sum = this.sums.get(destination);
+                if (sum == null) {
+                    return 0L;
+                }
+
+                // ロック結合。
+                sum.lock();
+            } finally {
+                this.lock.unlock();
+            }
+
+            final long cur = System.currentTimeMillis();
+            sum.trim(cur - this.duration);
+            if (sum.isEmpty()) {
+                empty = true;
+            } else if (this.sizeLimit <= sum.getSize() || this.countLimit <= sum.getCount()) {
+                sleep = sum.getFirstDate() + this.duration - cur // 一番古いのが削除されるまでの時間。
+                        + this.penalty;
+            }
+        } finally {
+            if (sum != null) {
+                sum.unlock();
+            }
+        }
+
+        if (empty) {
+            // 空なので削除。
+            this.lock.lockInterruptibly();
+            try {
+                sum = this.sums.get(destination);
+                if (sum != null) {
+                    sum.lock();
+                    try {
+                        if (sum.isEmpty()) {
+                            this.sums.remove(destination);
+                        }
+                    } finally {
+                        sum.unlock();
+                    }
+                }
+            } finally {
+                this.lock.unlock();
+            }
+        }
+
+        return sleep;
+    }
+
+    @Override
+    public void remove(final InetSocketAddress destination) throws InterruptedException {
+        long sleep = 0L;
+
+        Sum sum = null;
+        try { // 個別のロック。
+            this.lock.lockInterruptibly();
+            try { // 全体のロック。
+                sum = this.sums.get(destination);
+                if (sum == null) {
+                    // 解放済み。
+                    return;
+                }
+
+                // ロック結合。
+                sum.lock();
+            } finally {
+                this.lock.unlock();
+            }
+
+            final long cur = System.currentTimeMillis();
+            sum.trim(cur - this.duration);
+            if (!sum.isEmpty()) {
+                sleep = sum.getLastDate() + this.duration - cur; // 今あるのが全部削除されるまでの時間。
+            }
+        } finally {
+            if (sum != null) {
+                sum.unlock();
+            }
+        }
+
+        if (sleep > 0) {
+            LOG.log(Level.FINEST, "削除待ちで {0} ミリ秒さぼります。", Long.toString(sleep));
+            Thread.sleep(sleep);
+        }
+
+        this.lock.lockInterruptibly();
+        try {
+            sum = this.sums.get(destination);
+            if (sum != null) {
+                sum.lock();
+                try {
+                    final long cur = System.currentTimeMillis();
+                    sum.trim(cur - this.duration);
+                    if (sum.isEmpty()) {
+                        this.sums.remove(destination);
+                    }
+                } finally {
+                    sum.unlock();
+                }
+            }
+        } finally {
+            this.lock.unlock();
+        }
+    }
+
+}
---
+++ src/nippon/kawauso/chiraura/messenger/TrafficLimiter.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package nippon.kawauso.chiraura.messenger;
+
+import java.net.InetSocketAddress;
+
+/**
+ * 通信量・通信回数を制限するため。
+ * nextSleep が 0 の場合だけ通信して良い。
+ * @author chirauraNoSakusha
+ */
+interface TrafficLimiter {
+
+    /**
+     * 通信サイズを報告して必要な通信自粛時間を得る。
+     * @param size 通信したサイズ
+     * @param destination 通信先
+     * @return 自粛期間。自粛期間 (ミリ秒)。自粛しなくて良い場合は 0
+     * @throws InterruptedException 割り込まれた場合
+     */
+    long nextSleep(long size, InetSocketAddress destination) throws InterruptedException;
+
+    /**
+     * 必要な通信自粛時間を得る。
+     * @param size 通信したサイズ
+     * @param destination 通信先
+     * @return 自粛期間 (ミリ秒)。自粛しなくて良い場合は 0
+     * @throws InterruptedException 割り込まれた場合
+     */
+    long nextSleep(InetSocketAddress destination) throws InterruptedException;
+
+    /**
+     * 必要なければ、destination に関するリソースを解放する。
+     * @param destination 通信先
+     * @throws InterruptedException 割り込まれた場合
+     */
+    void remove(InetSocketAddress destination) throws InterruptedException;
+
+}
