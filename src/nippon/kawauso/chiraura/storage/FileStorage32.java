aa9d8d6be85bf16ba93d25d5beeaa037f9599726
chirauraNoSakusha <chiraura@outlook.jp>
2014-03-30 16:41:10 +0900

    Base32 を使う FileStorage を追加

---
+++ src/nippon/kawauso/chiraura/storage/FileStorage32.java
@@ -0,0 +1,38 @@
+package nippon.kawauso.chiraura.storage;
+
+import java.io.File;
+
+import nippon.kawauso.chiraura.lib.base.Address;
+import nippon.kawauso.chiraura.lib.converter.Base32;
+import nippon.kawauso.chiraura.lib.exception.MyRuleException;
+
+/**
+ * 操作毎にファイルを読み書きする倉庫。
+ * @author chirauraNoSakusha
+ */
+final class FileStorage32 extends FileStorage {
+
+    /*
+     * データ片は、その論理位置を BASE32 エンコードした名前のファイルに保存する。
+     */
+
+    FileStorage32(final File root, final int fileSizeLimit, final int directoryBitSize) {
+        super(root, fileSizeLimit, directoryBitSize);
+
+        final int maxDirectoryBitSize = (Address.SIZE / 5) * 5;// ディレクトリ名が論理位置以外の影響を受けない長さ。
+        if (maxDirectoryBitSize < directoryBitSize) {
+            throw new IllegalArgumentException("Too large directory bit size ( " + directoryBitSize + " ) not in [ 1, " + maxDirectoryBitSize + " ].");
+        }
+    }
+
+    @Override
+    String toFileString(final byte[] bytes) {
+        return Base32.toBase32(bytes);
+    }
+
+    @Override
+    byte[] fromFileString(final String string) throws MyRuleException {
+        return Base32.fromBase32(string);
+    }
+
+}
---
+++ src/nippon/kawauso/chiraura/storage/FileStorage32Test.java
@@ -0,0 +1,90 @@
+package nippon.kawauso.chiraura.storage;
+
+import java.io.File;
+
+import nippon.kawauso.chiraura.lib.math.MathFunctions;
+import nippon.kawauso.chiraura.lib.test.TestFunctions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author chirauraNoSakusha
+ */
+public final class FileStorage32Test {
+
+    private final File root;
+    private final int chunkSizeLimit;
+    private final int directoryBitSize;
+
+    /**
+     * 初期化。
+     */
+    public FileStorage32Test() {
+        this.root = new File(System.getProperty("java.io.tmpdir") + File.separator + FileStorage32Test.class.getName() + File.separator + System.nanoTime());
+
+        this.chunkSizeLimit = 1024 * 4;
+        this.directoryBitSize = 7;
+        TestFunctions.testLogging(this.getClass().getName());
+        // TestFunctions.testLogging(Level.ALL, Level.OFF);
+    }
+
+    private void checkTrash() {
+        final File trash = new File(this.root, "%%trash%%");
+        final File[] files = trash.listFiles();
+        Assert.assertArrayEquals(new File[0], files);
+    }
+
+    /**
+     * @throws Exception 異常
+     */
+    @Test
+    public void testMinimum() throws Exception {
+        // LoggingFunctions.startDebugLogging();
+        StorageTest.testMinimum(new FileStorage32(this.root, this.chunkSizeLimit, this.directoryBitSize));
+        checkTrash();
+    }
+
+    /**
+     * @throws Exception 異常
+     */
+    @Test
+    public void testRandom() throws Exception {
+        final int numOfLoops = 100_000;
+        final int numOfChunks = 100;
+        StorageTest.testRandom(new MemoryStorage(), new FileStorage32(this.root, this.chunkSizeLimit, this.directoryBitSize), numOfLoops, numOfChunks);
+        checkTrash();
+    }
+
+    private static final String PREFIX = FileStorage.class.getName() + ":";
+
+    /**
+     * @throws Exception 異常
+     */
+    @Test
+    public void testConcurrencyPerformanceByConstantChunk() throws Exception {
+        final int numOfLoops = 100;
+        final int numOfChunks = 100;
+        final int numOfProcesses = 1_000;
+        final int chunkSize = 2 * (int) ((MathFunctions.log2(numOfChunks) + Byte.SIZE - 1) / Byte.SIZE);
+        // LoggingFunctions.startLogging(Level.SEVERE);
+        StorageTest.testConcurrencyPerformanceByConstantChunk(new FileStorage32(this.root, this.chunkSizeLimit, this.directoryBitSize), numOfLoops,
+                numOfChunks,
+                numOfProcesses, chunkSize, PREFIX);
+        checkTrash();
+    }
+
+    /**
+     * @throws Exception 異常
+     */
+    @Test
+    public void testConcurrencyByVariableChunk() throws Exception {
+        final int numOfLoops = 100;
+        final int numOfChunks = 100;
+        final int numOfProcesses = 1_000;
+        StorageTest.testConcurrencyByVariableChunk(new FileStorage32(this.root, this.chunkSizeLimit, this.directoryBitSize), numOfLoops, numOfChunks,
+                numOfProcesses, PREFIX);
+        checkTrash();
+    }
+
+}
